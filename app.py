e3d12c8e572aedc3dcaa8271cd02e9c1b13ff690
MichelleC97 <MIchellec401@nycstudents.net>
2025-04-26 12:08:01 -0700

    save

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Sample movie data
-movies = [
-    {"id": 1, "title": "Avengers: Endgame", "price": 12},
-    {"id": 2, "title": "Spider-Man: No Way Home", "price": 10},
-    {"id": 3, "title": "Inception", "price": 8}
+about_me = [
+    {"Name": "Mich", "Age": "14", "Fav Color": "Pink and Teal"},
+    {"Sibling": "Viv", "Fav Color": "Red"}
 ]
 
 # Route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html', movies=movies)
+    return render_template('index.html', about_me=about_me)
 
 app.run(debug=True)
