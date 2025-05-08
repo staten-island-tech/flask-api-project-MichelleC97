6976f10b2671ede1ac557c1e24f4aa6d3b3e25f1
MichelleC97 <MIchellec401@nycstudents.net>
2025-05-08 09:38:39 -0700

    commit

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 # Sample movie data
 about_me = [
-    {"id": "My", "Name": "Mich", "Age": 14, "Fav Color": "Pink and Teal"},
-    {"id": "Sibling", "Sibling": "Viv", "Age": 16, "Fav Color": "Red"}, 
+    {"Title": "Myself", "Description": "I am a freshmen in Highschool. My favorite color is pink and teal. I LOVE the subject math. Summer is my favorite season because my brithday is during that time!"}, 
+    {"Title": "Sibling", "Description": "I have a sister name Viv, she is 2 years older than me. I like to annoy her (A LOT) during my free time. She goes to Susan E. Wagner High School."}, 
+    {"Title": "Hobby", "Description": "I like to play with my family and friends. My favorite sport is Badminton. I don't hate dancing but it's not my favorite as well. I also want to have a dog so badly!!"},
 ]
 
 # Route for the home page
